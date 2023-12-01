bec76867d8735513257e622400a6dde80e20cc0e
aljan2000 <aljan2000@mail.ru>
2023-12-01 06:20:06 +0600

    1

---
+++ src/pages/shop/shop.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const shop = () => {
+    return <div className="shop">
+        <div className="shopTitle">
+            <h1> TactiCool Shop </h1>
+        </div>
+        <div className="products">
+
+        </div>
+    </div>;
+}
---
+++ src/products.js
@@ -0,0 +1,24 @@
+import prd1 from './assets/gloves.jpg';
+import prd2 from './assets/2.jpg';
+import prd3 from './assets/3.jpg';
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: "Tactical Gloves by Splav",
+        price: 3500.00,
+        productImage: prd1,
+    },
+    {
+            id: 2,
+            productName: "M65 Woodland Cold Uniform",
+            price: 5000.00,
+            productImage: prd2,
+    },
+    {
+            id: 3,
+            productName: "Gorka ATACS-FG Winter Uniform",
+            price: 5000.00,
+            productImage: prd3,
+    },
+];
